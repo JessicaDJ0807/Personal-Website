e15dfc25b152f2f2b47c7530e49e66270cd74b88
AlbertLin0327 <linhsinkai@gmail.com>
2021-08-15 22:19:03 +0800

    Updates

---
+++ precache-manifest.a47dab91b15fe9656ae6f2d515317f45.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83414b4eb2dac60d429267877a50a6c8",
+    "url": "https://albertlin0327.github.io/index.html"
+  },
+  {
+    "revision": "3712d04fcac868f413e6",
+    "url": "https://albertlin0327.github.io/static/css/2.52bc9db8.chunk.css"
+  },
+  {
+    "revision": "b70f6527b62511ac40f3",
+    "url": "https://albertlin0327.github.io/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "3712d04fcac868f413e6",
+    "url": "https://albertlin0327.github.io/static/js/2.f9726e1c.chunk.js"
+  },
+  {
+    "revision": "fd27049dfa9d84d31f372f995e8edb49",
+    "url": "https://albertlin0327.github.io/static/js/2.f9726e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70f6527b62511ac40f3",
+    "url": "https://albertlin0327.github.io/static/js/main.f68abb5f.chunk.js"
+  },
+  {
+    "revision": "991e806bef6e8a7d257b",
+    "url": "https://albertlin0327.github.io/static/js/runtime-main.5be2cc22.js"
+  },
+  {
+    "revision": "761e89ae0b73b0a9ac8694b339e3e1b0",
+    "url": "https://albertlin0327.github.io/static/media/Albert_Lin.761e89ae.jpg"
+  },
+  {
+    "revision": "2c50d7a0caadcdd36b063bd9f9268291",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_AMS-Regular.2c50d7a0.woff2"
+  },
+  {
+    "revision": "3392cf39311b42f70073c2732b99a24c",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_AMS-Regular.3392cf39.woff"
+  },
+  {
+    "revision": "342a61e0c4dee016881d00d9fa69c335",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_AMS-Regular.342a61e0.ttf"
+  },
+  {
+    "revision": "0f0d0935d51deac6c1f7f0dea7ffe886",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Bold.0f0d0935.woff2"
+  },
+  {
+    "revision": "9ae9058d9d744f501b41d1fe1236ed57",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Bold.9ae9058d.woff"
+  },
+  {
+    "revision": "b27e354b9a940cfabb1cdad2f5d35eb9",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Bold.b27e354b.ttf"
+  },
+  {
+    "revision": "8a3d1df80c7d8618896f166336d89dce",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Regular.8a3d1df8.woff2"
+  },
+  {
+    "revision": "bd18bae257a88cfa6a36310cd585d0a0",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Regular.bd18bae2.ttf"
+  },
+  {
+    "revision": "f7eb2b22569f8c8a9ad8b6d299d4f6a4",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Caligraphic-Regular.f7eb2b22.woff"
+  },
+  {
+    "revision": "314623ce54a8a2654fb5df3c9cec01aa",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Bold.314623ce.woff"
+  },
+  {
+    "revision": "359e1e974d07f534d0665dc5add72209",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Bold.359e1e97.ttf"
+  },
+  {
+    "revision": "e2c9ac8504a2e0f1ec22e5904087174f",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Bold.e2c9ac85.woff2"
+  },
+  {
+    "revision": "6b53a2db48b3bbde91ec9c85590ccc44",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Regular.6b53a2db.ttf"
+  },
+  {
+    "revision": "98d5da581ba8efea0934c92794c6c56c",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Regular.98d5da58.woff"
+  },
+  {
+    "revision": "a2be00f37608cf99fb1c0351dc4e9342",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Fraktur-Regular.a2be00f3.woff2"
+  },
+  {
+    "revision": "233f218ee616854f491c7195cd8fffcf",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Bold.233f218e.woff2"
+  },
+  {
+    "revision": "316611c7a4afe59d3bcbe9e0f1a41613",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Bold.316611c7.woff"
+  },
+  {
+    "revision": "ed829b5fabfded4f5f943df1012da1b1",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Bold.ed829b5f.ttf"
+  },
+  {
+    "revision": "09fa2c8f6a5a787648fcf62022b9e5c6",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-BoldItalic.09fa2c8f.woff2"
+  },
+  {
+    "revision": "ca23ba4b607268345b1f0b22bff41292",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-BoldItalic.ca23ba4b.ttf"
+  },
+  {
+    "revision": "cd5eb9a8d163f765f01c4b1a44567f4c",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-BoldItalic.cd5eb9a8.woff"
+  },
+  {
+    "revision": "14ff9c98a820d988c169e27ca3eb78b8",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Italic.14ff9c98.ttf"
+  },
+  {
+    "revision": "4dc3271d3627578d951ecd3fcdb7f762",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Italic.4dc3271d.woff2"
+  },
+  {
+    "revision": "4eee467e52c8113a7c48549dec426808",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Italic.4eee467e.woff"
+  },
+  {
+    "revision": "6c094af9274ceccdc817400fdf49c5af",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Regular.6c094af9.woff2"
+  },
+  {
+    "revision": "ab80a08c3c6915afb4b4a3300a55ef8f",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Regular.ab80a08c.woff"
+  },
+  {
+    "revision": "c89c643655fabf99f2b78eb9cc5cf4b0",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Main-Regular.c89c6436.ttf"
+  },
+  {
+    "revision": "7b481bb892d872c27234188b59ca8f10",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-BoldItalic.7b481bb8.ttf"
+  },
+  {
+    "revision": "8bdd60921916168d5c53c2caf3931e86",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-BoldItalic.8bdd6092.woff2"
+  },
+  {
+    "revision": "e1a2ff93bd15ea54164340e3d1b2f901",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-BoldItalic.e1a2ff93.woff"
+  },
+  {
+    "revision": "1aa96e4e339118aad4153041adbd9947",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-Italic.1aa96e4e.woff2"
+  },
+  {
+    "revision": "68f671df4bc4ab87b9f3e5111c28c49a",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-Italic.68f671df.woff"
+  },
+  {
+    "revision": "f677173e00e38ef3151aa3156ef87507",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Math-Italic.f677173e.ttf"
+  },
+  {
+    "revision": "362d94c68887ef3079e3fa04abc7b505",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Bold.362d94c6.ttf"
+  },
+  {
+    "revision": "f1cea170c77d231b4ce249fcf850f3f4",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Bold.f1cea170.woff"
+  },
+  {
+    "revision": "f9ae435239a0933219b7dba4480141cc",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Bold.f9ae4352.woff2"
+  },
+  {
+    "revision": "07ffaec68ea2d7c3428eec8b12969925",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Italic.07ffaec6.woff"
+  },
+  {
+    "revision": "2c74297808a50220aa64d435d69006bc",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Italic.2c742978.ttf"
+  },
+  {
+    "revision": "e7aabbc7a3b86d947d8ad4436a81b348",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Italic.e7aabbc7.woff2"
+  },
+  {
+    "revision": "6087fc040fbe6c4e83da5ee2c1f8a803",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Regular.6087fc04.ttf"
+  },
+  {
+    "revision": "6f7840dc539b8786da712dc43a57051c",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Regular.6f7840dc.woff2"
+  },
+  {
+    "revision": "e8735df90d494a81dfc1568c352b21bc",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_SansSerif-Regular.e8735df9.woff"
+  },
+  {
+    "revision": "5921f5ba7c02f3aeb478e4e87d2187bf",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Script-Regular.5921f5ba.woff2"
+  },
+  {
+    "revision": "781730b2f066b5268a8fb75b4f7c479b",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Script-Regular.781730b2.ttf"
+  },
+  {
+    "revision": "ced4ee620859978fa1921c87d6dbd774",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Script-Regular.ced4ee62.woff"
+  },
+  {
+    "revision": "3a5edc1620fc7a7a2e25112b7a8e6cf8",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size1-Regular.3a5edc16.woff2"
+  },
+  {
+    "revision": "54a80b37e92f14f32a33165c571f8e95",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size1-Regular.54a80b37.ttf"
+  },
+  {
+    "revision": "aec6c2103821f67bc9f53dee3fbad982",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size1-Regular.aec6c210.woff"
+  },
+  {
+    "revision": "0d72ed102d351b453046ee3d9c4600fb",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size2-Regular.0d72ed10.woff"
+  },
+  {
+    "revision": "24cbe093e557076be8c6c5ffee4aa61b",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size2-Regular.24cbe093.ttf"
+  },
+  {
+    "revision": "963980610d2663b7c04355bcf17d5bb8",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size2-Regular.96398061.woff2"
+  },
+  {
+    "revision": "34ab65d6e933eaa143598aa54b5832d6",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size3-Regular.34ab65d6.woff"
+  },
+  {
+    "revision": "920b3023bec41ddd8a7d79ff9da70643",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size3-Regular.920b3023.woff2"
+  },
+  {
+    "revision": "ee3e5bf4b1e08bb325312348661c276b",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size3-Regular.ee3e5bf4.ttf"
+  },
+  {
+    "revision": "04dc70fcc38cd220b3129133310717ca",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size4-Regular.04dc70fc.woff"
+  },
+  {
+    "revision": "83af4078518f26c4b62160f6565d6d86",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size4-Regular.83af4078.woff2"
+  },
+  {
+    "revision": "b78c75bb4d0c95e4a87e006e83f187e4",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Size4-Regular.b78c75bb.ttf"
+  },
+  {
+    "revision": "8073cf012f453ddf47d5b8776f6bb1e5",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Typewriter-Regular.8073cf01.woff2"
+  },
+  {
+    "revision": "90f78c1075e0cdb56287589112f56011",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Typewriter-Regular.90f78c10.ttf"
+  },
+  {
+    "revision": "b636fa487ef5e2f56bb3529bc9168be0",
+    "url": "https://albertlin0327.github.io/static/media/KaTeX_Typewriter-Regular.b636fa48.woff"
+  },
+  {
+    "revision": "a941776cfbf8436cdb01ea1d2c5c6bef",
+    "url": "https://albertlin0327.github.io/static/media/resume.a941776c.pdf"
+  }
+]);
